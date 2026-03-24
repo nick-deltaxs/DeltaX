73768a6a48f66fbda6213f1f6780167147157128
Craftzmesz <kadekwidisuandana@gmail.com>
2026-03-25 06:38:08 +0800

    W3-Middleware: built by NewMember

--- Codebase/middleware.ts
+++ Codebase/middleware.ts
@@ -22,6 +22,6 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|api/og).*)",
+    "/((?!_next/static|_next/image|favicon.ico|api/og|.*\\.).*)",
   ],
 };
